251711c3897b113e2fabbd9c03ced9f01a526740
Alik <jesus.cuentas@unisucrevirtual.edu.co>
2025-09-24 06:57:53 +0000

    ✅ Actualizar nombres de constantes para las URIs de proveedores en assets.ts y embed-serv.ts

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,5 +1,5 @@
-const PROVIDERS_URI = './assets/providers.json';
-const CAT_FRAME = './assets/sc-logging.js';
-const SET_CORE_URI = './assets/set-core.json';
+const PROVIDERS_JSON = '/assets/providers.json';
+const CAT_FRAME = '/assets/sc-logging.js';
+const SET_CORE_URI = '/assets/set-core.json';
 
-export { PROVIDERS_URI, CAT_FRAME, SET_CORE_URI };
+export { PROVIDERS_JSON, CAT_FRAME, SET_CORE_URI };
