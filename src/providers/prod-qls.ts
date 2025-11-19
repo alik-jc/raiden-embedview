b9213752fc6440a4d6cc38d27dc3ecee0fa7403d
Jesus Cuentas <jesus.cuentas@unisucrevirtual.edu.co>
2025-11-19 20:54:24 +0000

    ✅ Actualizar proveedores y eliminar URLs obsoletas en set-core.json

--- src/providers/prod-qls.ts
+++ src/providers/prod-qls.ts
@@ -1,22 +1,43 @@
 import { SET_CORE_URI } from '../index';
 
-export const qlsProvider = async (uriParameter: string) => {
+export const luluProd = async (uriParameter: string) => {
     const json = SET_CORE_URI
-        const urlSet = json.qls;
-        const response = urlSet + uriParameter;
-        return response;
+        const urlSet = json.lulu;
+
+        const hashMatch = uriParameter.match(/\/e\/([^\/]+)/);
+        const hash = hashMatch ? hashMatch[1] : '';
+    
+        const urlResponse = hash
+            ? urlSet + hash
+            : urlSet + '/' + uriParameter;
+    
+        return urlResponse;
 };
 
-export const uqlsProvider = async (uriParameter: string) => {
-    const json = SET_CORE_URI
-        const urlSet = json.uqls;
-        const response = urlSet + uriParameter;
-        return response;
+export const uqloProd = async (uriParameter: string) => {
+    const json = SET_CORE_URI;
+    const urlSet = json.uqload;
+    
+    const hashMatch = uriParameter.match(/https?:\/\/[^\/]+\/([^\/]+)/);
+    const hash = hashMatch ? hashMatch[1] : '';
+
+    const urlResponse = hash
+        ? urlSet + hash
+        : urlSet + '/' + uriParameter;
+
+    return urlResponse;
 }
 
-export const setProvider = async (uriParameter: string) => {
+export const fmoonProd = async (uriParameter: string) => {
     const json = SET_CORE_URI;
-    const urlSet = json.url;
-    const response = urlSet + uriParameter;
-    return response;
+    const urlSet = json.fmoon;
+
+    const hashMatch = uriParameter.match(/\/e\/([^\/]+)/);
+    const hash = hashMatch ? hashMatch[1] : '';
+
+    const urlResponse = hash
+        ? urlSet + hash
+        : urlSet + '/' + uriParameter;
+
+    return urlResponse;
 };
