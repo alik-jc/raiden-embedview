ebefe3c4f181761e52ea593477c665c145f63e49
alik-jc <jesusdaniel723@gmail.com>
2025-12-13 16:42:18 -0500

    feat: integrate dotenv to load all process environment variables into PM2 app configurations.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,31 @@
+require('dotenv').config();
+
 module.exports = {
-  apps: [{
-    name: 'Embed-View-Serv',
-    script: 'dist/embed-serv.js',
-    interpreter: 'bun',
-    instances: 1,
-    exec_mode: 'fork',
-    watch: false,
-    max_memory_restart: '500M',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    },
-    error_file: './logs/pm2-error.log',
-    out_file: './logs/pm2-out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    merge_logs: true,
-    autorestart: true,
-    max_restarts: 10,
-    min_uptime: '10s',
-    restart_delay: 4000
-  }]
+    apps: [{
+        name: 'Embed-View-Serv',
+        script: 'dist/embed-serv.js',
+        interpreter: 'bun',
+        instances: 1,
+        exec_mode: 'fork',
+        watch: false,
+        max_memory_restart: '500M',
+        env: {
+            NODE_ENV: 'development',
+            // Carga todas las variables del .env para desarrollo
+            ...process.env
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            // Carga todas las variables del .env para producción
+            ...process.env
+        },
+        error_file: './logs/pm2-error.log',
+        out_file: './logs/pm2-out.log',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        merge_logs: true,
+        autorestart: true,
+        max_restarts: 10,
+        min_uptime: '10s',
+        restart_delay: 4000
+    }]
 };
